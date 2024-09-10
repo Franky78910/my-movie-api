ffd13d26ed4fc48c3f2d39e1d4c1222c908edddf
Franklin <frankyr@misena.edu.co>
2024-09-09 20:38:33 -0500

    Abordamos hasta la sesion 2 de fastAPI

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 app = FastAPI()
-app.title = "Mi Aplicacion En FastAPI"
-@app.get('/')
+app.title = "Mi aplicacion con FastAPI"
+app.version = "0.0.1"
+movies_list = [
+    {
+        "id": 1, 
+        "title": "Deap POOL",
+        "overview": "Comica Infantil",
+        "year": 2024,
+        "rating": 100.0
+    },
+    {
+        "id": 2,
+        "title": "Batman",
+        "overview": "Comica",
+        "year": 2011,
+        "rating": 200.0
+    }
+    
+]
+@app.get('/', tags=["Home"])
 def message():
-    return "Hello World"
+    return HTMLResponse ('<h1> Hello world </h1>')
+@app.get('/movies', tags=["Movies"])
+def get_movies():
+    return movies_list
+
+@app.get('/movies/{id}' , tags=["Movies"])
+def get_movie(id: int):
+    for item in movies_list:
+        if item["id"] == id:
+            return item
+    return []    
